5d1619a14270043c5c30e188b98514f32f4e8e4c
polina <polina.sor123@gmail.com>
2017-07-26 12:56:15 +0300

    Draw sprites on screen

--- snake/snake.py
+++ snake/snake.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from pygame.locals import *
 
@@ -5,6 +6,31 @@ WIDTH = 500
 HEIGHT = 500
 
 WHITE = (255, 255, 255)
+
+def load_png(png):
+    fullname = os.path.join(png)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as err:
+        print("Can't load image.")
+        raise SystemExit(err.args[0])
+    image = image.convert_alpha()
+    return image, image.get_rect()
+
+
+class Segment(pygame.sprite.Sprite):
+    def __init__(self, xy):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png('segment.png')
+        self.rect.topleft = xy
+
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, xy):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png('apple.png')
+        self.rect.topleft = xy
+
 
 def main():
     pygame.init()
@@ -16,7 +42,13 @@ def main():
     background = background.convert()
     background.fill(WHITE)
 
+    segm = Segment([20, 20])
+    apple = Apple([100, 100])
+
+    sprites = pygame.sprite.Group(segm, apple)
+
     screen.blit(background, (0, 0))
+    sprites.draw(screen)
     pygame.display.flip()
 
     while 1:
